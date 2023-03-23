c36075b6cc6b1969b722b87586ac36eee1ca4d03
Julio Leon <juliopena143@icloud.com>
2023-03-23 16:23:48 -0600

    create

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,4 +8,34 @@ dotenv.config();
 
 const router = express.Router();
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_key: process.env.CLOUDINARY_CLOUD_KEY,
+    cloud_secret: process.env.CLOUDINARY_CLOUD_SECRET,
+});
+
+// GET ALL POSTS
+router.route('/').get(async (req, res) => {
+
+
+})
+
+// CREATE POST
+router.route('/').get(async (req, res) => {
+    try {
+        const { name, prompt, photo } = req.body;
+        const photoUrl = await cloudinary.uploader.upload(photo);
+
+        const newPost = await Post.create({
+            name,
+            prompt,
+            photo: photoUrl.url,
+        })
+
+        res.status(201).json({ success: true, data: newPost });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+});
+
 export default router;
